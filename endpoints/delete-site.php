38afc5c0c65444e2bff114db5c8d0ad76183a473
Michael Standen <screaminghawk@gmail.com>
2016-09-22 10:14:59 +1200

    Added deleting site by store_id

--- endpoints/delete-site.php
+++ endpoints/delete-site.php
@@ -7,7 +7,7 @@ $api = new Multisite_JSON_API\Endpoint();
 /*
  * Make sure we are given the correct JSON
  */
-if(isset($api->json->blog_id)) {
+if(isset($api->json->blog_id) || isset($api->json->store_id)) {
 	if(!isset($api->json->drop))
 		$api->json->drop = false;
 
@@ -28,8 +28,30 @@ if(isset($api->json->blog_id)) {
 		 */
 		} else {
 			// Start killing stuff
+			if (isset($api->json->blog_id)){
+				$blog_id = $api->json->blog_id;
+			} else if (isset($api->json->store_id)){
+				$blog_id = $wpdb->get_var($wpdb->prepare("SELECT site_id FROM store_wp_site_mapping WHERE store_id = %d", $api->json->store_id));
+				if (is_null($blog_id)){
+					$api->error('Unable to find blog from store_id', 400);
+					die();
+				}
+			} else {
+				$api->error('No blog_id or store_id provided', 400);
+				die();
+			}
 			try {
-				$site = $api->delete_site($api->json->blog_id, $api->json->drop);
+				$site = $api->delete_site($blog_id, $api->json->drop);
+				
+				try {
+					$wpdb->delete('store_wp_site_mapping', 
+						array('site_id' => $blog_id), 
+						array('%d')
+					);
+				} catch (Exception $e){
+					// Must not be used. Ignore.
+				}
+				
 				$api->respond_with_json($site, 202);
 			} catch(MultiSite_JSON_API\SiteNotFoundException $e) {
 				$api->json_exception($e);
@@ -41,6 +63,6 @@ if(isset($api->json->blog_id)) {
 		die();
 	}
 } else {
-	$api->error('This endpoint needs a JSON payload of the form {"blog_id": 1, "drop": true}');
+	$api->error('This endpoint needs a JSON payload of the form {"blog_id": 1, "drop": true}', 400);
 }
 ?>
